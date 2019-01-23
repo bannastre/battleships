126425682a557136144f3d63e8c899ab4563501c
Chris Harrop <chris@jigsaw.xyz>
2019-01-23 15:16:46 +0000

    Create a Board of length * width

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,20 @@
 class Board:
-  def __init__(self, height, width):
-    self.height = height
+  def __init__(self, width, height):
     self.width = width
+    self.height = height
+    self.squares = []
+    self.populate_squares()
+
+  def populate_squares(self):
+    number_of_rows = 0
+    while number_of_rows < (self.height):
+      number_of_squares = 0
+      row = []
+      while number_of_squares < (self.width):
+        row.extend([False])
+        number_of_squares += 1
+      self.squares.extend([row])          
+      number_of_rows += 1
+  
+  def is_square_full(self, x, y):
+    return self.squares[self.height-1][self.width-1]
--- test/test_board.py
+++ test/test_board.py
@@ -1,7 +1,16 @@
 import pytest
 from lib.models import Board
 
-def test_board_location():
+def test_board():
   board = Board(6,9)
-  assert board.height == 6
-  assert board.width == 9
+  assert board.width == 6
+  assert board.height == 9
+
+def test_populate_squares():
+  board = Board(4,3)
+  assert len(board.squares) == 3
+  assert len(board.squares[0]) == 4
+
+def test_is_square_full():
+  board = Board(4,3)
+  assert board.is_square_full(2,2) == False
